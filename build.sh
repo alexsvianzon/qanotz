567d95cc728f20d1e4edaa11b176e9e5790fd675
alexsvianzon <alexsvianzon@gmail.com>
2026-02-22 10:57:45 -0600

    Change ICON_ARG

--- build.sh
+++ build.sh
@@ -28,7 +28,7 @@ case "$PLATFORM" in
     ;;
 esac
 
-pyinstaller --noconsole -n QANotz qanotz/__main__.py --paths . --icon=ICON_ARG --clean
+pyinstaller --noconsole -n QANotz qanotz/__main__.py --paths . --icon=$ICON_ARG --clean
 
 case "$PLATFORM" in
   macos)
