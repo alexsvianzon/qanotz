1db0957aa22cd48728674e500da16d29e0927367
pixel.pine <alexsvianzon@gmail.com>
2026-02-24 17:19:24 +0000

    Add quick debugging

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import os
 import sys
 
-print("--- DEBUG BUNDLE STRUCTURE ---")
-if getattr(sys, 'frozen', False):
-    print(f"Bundle Root: {sys._MEIPASS}") # pyright: ignore[reportAttributeAccessIssue]
-    for root, dirs, files in os.walk(sys._MEIPASS): # pyright: ignore[reportAttributeAccessIssue]
-        level = root.replace(sys._MEIPASS, '').count(os.sep) # pyright: ignore[reportAttributeAccessIssue]
-        indent = ' ' * 4 * (level)
-        print(f"{indent}{os.path.basename(root)}/")
-        subindent = ' ' * 4 * (level + 1)
-        for f in files:
-            print(f"{subindent}{f}")
-print("--- END DEBUG ---")
+DEBUGGING = False
+
+if DEBUGGING:
+    print("--- DEBUG BUNDLE STRUCTURE ---")
+    if getattr(sys, 'frozen', False):
+        print(f"Bundle Root: {sys._MEIPASS}") # pyright: ignore[reportAttributeAccessIssue]
+        for root, dirs, files in os.walk(sys._MEIPASS): # pyright: ignore[reportAttributeAccessIssue]
+            level = root.replace(sys._MEIPASS, '').count(os.sep) # pyright: ignore[reportAttributeAccessIssue]
+            indent = ' ' * 4 * (level)
+            print(f"{indent}{os.path.basename(root)}/")
+            subindent = ' ' * 4 * (level + 1)
+            for f in files:
+                print(f"{subindent}{f}")
+    print("--- END DEBUG ---")
 
 from qanotz.__main__ import main
 
